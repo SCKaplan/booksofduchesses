dfe9f14eb265cd74f6ed555250c32d5f01b4f345
Freddie Gould <FreddieGould@Freddies-MacBook-Air.local>
2019-06-06 14:09:46 -0400

    books, etc. model functioning

--- books_app/models.py
+++ books_app/models.py
@@ -9,66 +9,26 @@ class Tag(models.Model):
 class Book(models.Model):
 	title = models.CharField(max_length=200)
 	author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-	owner = models.ManyToManyField('DateOwned', blank=True)
+	owner = models.ManyToManyField('Owner', blank=True)
 	type = models.CharField(max_length=200)
 	date = models.CharField(max_length=30)
 	language = models.CharField(max_length=30)
+	def __str__(self):
+		return self.title
 
 class Author(models.Model):
 	name = models.CharField(max_length=200)
+	def __str__(self):
+		return self.name
 
 class Owner(models.Model):
 	name = models.CharField(max_length=200)
+	book_date = models.ManyToManyField('DateOwned', blank=True)
+	#location = models.CharField(max_length=200) #This can be updated to include a location as a field
+	def __str__(self):
+		return self.name
 
 class DateOwned(models.Model):
-    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
-    book_owned = models.ForeignKey(Book, on_delete=models.CASCADE)
-    dateowned = models.DateTimeField()
-
-
-
-
-
-# class DateOwned(MultiValueField):
-# 	def __init__(self, **kwargs):
-# 		# Define one message for all fields.
-# 		error_messages = {
-#     		'incomplete': 'Enter an owner and a date of ownership.',
-# 			}
-# 		# Or define a different message for each field.
-# 		fields = (
-# 		    ManyToManyField('Owner'),
-# 		    DateTimeField(),
-# 		)
-# 		super().__init__(
-# 		    fields=fields,
-# 		    require_all_fields=False, **kwargs
-# 		)
-
-
-
-
-
-
-
-
-
-
-
-# class Text(models.Model):
-#     title = models.CharField(max_length=30)
-#     english_title = models.CharField(max_length=30)
-#     language = models.CharField(max_length=30)
-#     tags = models.ManyToManyField(Tag)
-#
-#
-# class TextinBook(models.Model):
-#     text = models.ForeignKey(Text, on_delete=models.CASCADE)
-#     book = models.ManyToManyField(Tag)
-#     page_range = models.CharField(max_length=30)
-#
-#
-# class Person(models.Model):
-# 	book_owner = models.CharField(max_length=200)
-# 	text_author = models.CharField(max_length=200)
-# 	book_author = models.CharField(max_length=200)
+	#owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+	#book_owned = models.ForeignKey(Book, on_delete=models.CASCADE)
+	dateowned = models.DateTimeField()
