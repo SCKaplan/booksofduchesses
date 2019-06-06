3c5fe5b532d614d641639fe9842eeaf6a3f0967a
Freddie Gould <FreddieGould@Freddies-MacBook-Air.local>
2019-06-06 14:26:51 -0400

    register models

--- books_app/admin.py
+++ books_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Tag, Author, Owner, DateOwned, Book
+
 # Register your models here.
+admin.site.register(Tag)
+admin.site.register(Author)
+admin.site.register(Owner)
+admin.site.register(DateOwned)
+admin.site.register(Book)
