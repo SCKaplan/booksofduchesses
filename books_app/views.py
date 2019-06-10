22f50880dfe38b8547d8851bef5f40ac951bfa82
Freddie Gould <FreddieGould@Freddies-MacBook-Air.local>
2019-06-10 09:13:55 -0400

    code to load db by running index

--- books_app/views.py
+++ books_app/views.py
@@ -18,3 +18,33 @@ def index(request):
         tmp.save()
 
     f.close()
+
+    authors = open('/File/Path/To Authors/.csv', 'r')
+    for line in authors:
+        line =  line.split(',')
+        tmp = Author.objects.create()
+        tmp.name = line[0]
+        tmp.save()
+
+    authors.close()
+
+    owners = open('/File/Path/To Owners/.csv', 'r')
+    for line in owners:
+        line =  line.split(',')
+        tmp = Owner.objects.create()
+        tmp.name = line[0]
+        tmp.motto = line[1]
+        tmp.symbol = line[2]
+        tmp.save()
+
+    owners.close()
+
+    text = open('/File/Path/To Texts/.csv', 'r')
+    for line in text:
+        line =  line.split(',')
+        tmp = Text.objects.create()
+        tmp.name = line[0]
+        tmp.name_eng = line[1]
+        tmp.save()
+
+    text.close()
