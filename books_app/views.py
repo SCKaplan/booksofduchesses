7f458708ae0aae2a1880e45bf9a63678331c3c05
fgould <43426151+fgould@users.noreply.github.com>
2019-06-14 14:40:52 -0400

    cleaning

--- books_app/views.py
+++ books_app/views.py
@@ -48,50 +48,7 @@ def index(request):
         search_form = SearchForm()
 
         return render(request, 'index.html',{'books':books, 'locations':locations, 'search_form': search_form, 'authors': authors, 'owners':owners})
-    # 
-    # f = open('/Users/FreddieGould/Downloads/books.csv', 'r')
-    # for line in f:
-    #     line =  line.split(',')
-    #     tmp = Book.objects.create()
-    #     tmp.title = line[0]
-    #     tmp.date_created = line[1]
-    #     tmp.scribes = line[2]
-    #     tmp.illuminators = line[3]
-    #     tmp.digital_version = line[4]
-    #     tmp.save()
-    # 
-    # f.close()
-    # 
-    # authors = open('/Users/FreddieGould/Downloads/authors.csv', 'r')
-    # for line in authors:
-    #     line =  line.split(',')
-    #     tmp = Author.objects.create()
-    #     tmp.name = line[0]
-    #     tmp.save()
-    # 
-    # authors.close()
-    # 
-    # owners = open('/Users/FreddieGould/Downloads/owners.csv', 'r')
-    # for line in owners:
-    #     line =  line.split(',')
-    #     tmp = Owner.objects.create()
-    #     tmp.name = line[0]
-    #     tmp.motto = line[1]
-    #     tmp.symbol = line[2]
-    #     tmp.save()
-    # 
-    # owners.close()
-    # 
-    # text = open('/Users/FreddieGould/Downloads/texts.csv', 'r')
-    # for line in text:
-    #     line =  line.split(',')
-    #     tmp = Text.objects.create()
-    #     tmp.name = line[0]
-    #     tmp.name_eng = line[1]
-    #     tmp.save()
-    # 
-    # text.close()
-   
+  
 def loadup(request):
     textinfo = open('/Path/To/CSV/texts2.csv', 'r')
     texts = Text.objects.all()
