f99492b177b0faa08c1d4dcd499f05cc65a8c631
fgould <43426151+fgould@users.noreply.github.com>
2019-06-14 15:02:00 -0400

    Update urls.py

--- booksofduchesses/urls.py
+++ booksofduchesses/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('books_app.urls')),
+    path('loadup/', include('books_app.urls')),
 ]
