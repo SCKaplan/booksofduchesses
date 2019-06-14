499566f517f5a4958ace49df06e415f498d1b012
root <apjanco@gmail.com>
2019-06-14 18:35:46 +0000

    Merge branch 'master' of https://github.com/HCDigitalScholarship/booksofduchesses
    Update remote

--- books_app/urls.py
+++ books_app/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import include
 from django.contrib import admin
-
-from . import views
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import include
+import books_app.views as views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('loadup/', views.loadup, name='loadup'),
 ]
