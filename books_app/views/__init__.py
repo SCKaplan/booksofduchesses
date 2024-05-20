58cb0fbbe3f5b47ae8c5bc3f7924f02e74da7061
Chad Simmons <chad.simmons@logicmonitor.com>
2024-05-20 14:45:16 -0700

    Fixed searching by autocomplete fields

--- books_app/views/__init__.py
+++ books_app/views/__init__.py
@@ -66,14 +66,15 @@ def index(request):
     # if a GET (or any other method) we'll create a blank form
     else:
         # Default display is all female owners
-        # owner_locations = [opdl.the_place for opdl in OwnerPlaceDateLived.objects.filter(owner__in=Owner.objects.filter(gender__exact="Female"))]
+        owner_qs = Owner.objects.filter(gender__exact="Female")
+        owner_locations = list(set(location.the_place for owner in owner_qs for location in owner.owner_location.all()))
         return render(
             request,
             "index.html",
             {
                 "books": [],
                 "search_form": SearchForm(),
-                "owners": [],
+                "owners": owner_locations,
                 "display_search": False,
                 "books_about": books_about,
                 "owners_about": owners_about,
